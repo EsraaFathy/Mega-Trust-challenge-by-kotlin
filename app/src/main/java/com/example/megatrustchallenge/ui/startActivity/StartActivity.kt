1df2b746305297a8f7267db58c635f7c2f0a64a0
Esraa Fathy <fesraa240@gmail.com>
2021-05-22 04:03:17 +0200

    all start screen

--- app/src/main/java/com/example/megatrustchallenge/ui/startActivity/StartActivity.kt
+++ app/src/main/java/com/example/megatrustchallenge/ui/startActivity/StartActivity.kt
@@ -1,12 +1,20 @@
 package com.example.megatrustchallenge.ui.startActivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.megatrustchallenge.R
+import com.example.megatrustchallenge.databinding.ActivityStartBinding
+import com.example.megatrustchallenge.ui.jobScreen.JobsScreen
 
 class StartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_start)
+        binding.startButton.setOnClickListener {
+            startActivity(Intent(this,JobsScreen::class.java))
+        }
     }
 }
