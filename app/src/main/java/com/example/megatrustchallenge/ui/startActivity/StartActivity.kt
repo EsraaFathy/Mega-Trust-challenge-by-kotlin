2addacf4769d4a694e98a43e5b463d234e5608bc
Esraa Fathy <fesraa240@gmail.com>
2021-05-22 03:47:33 +0200

    start screen design

---
+++ app/src/main/java/com/example/megatrustchallenge/ui/startActivity/StartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.megatrustchallenge.ui.startActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.megatrustchallenge.R
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
